6e1c60e69e4d42a8a5d8463b58c57d5e422503da
dalison <dalisonsr68@gmail.com>
2022-10-22 13:12:21 -0300

    Tela de usuario mostrando catálogo de todos os livros

--- view/tela_usuario.php
+++ view/tela_usuario.php
@@ -11,24 +11,37 @@
 </head>
 
 <body>
-    <div class="dropdown-center">
-        <button id="dropdown" class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="true">
-      <img id="icone-menu" src="../imagens/menu.png" alt="">
-    </button>
-        <ul class="dropdown-menu">
-            <li>
-                <a class="dropdown-item" href="#"><img class="icones" src="../imagens/conta.png" alt="">Minha conta</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="#"><img class="icones" src="../imagens/livro.png" alt="">Meus Livros</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="#"><img class="icones" src="../imagens/filtro.png" alt="">Categorias</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="#"><img class="icones" src="../imagens/sair.png" alt="">Sair</a>
-            </li>
-        </ul>
+    <div id="barra">
+        <div class="dropdown-center">
+            <button id="dropdown" class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="true">
+                <img class="icones-menu" src="../imagens/menu.png" alt="">
+            </button>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="dropdown-item" href="#"><img class="icones" src="../imagens/conta.png" alt="">Minha conta</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#"><img class="icones" src="../imagens/livro.png" alt="">Meus Livros</a>
+                </li>
+                <li id="categorias">
+                    <div class="dropdown-item">
+                        <div><img class="icones" src="../imagens/filtro.png" alt="">Categorias</div>
+                        <select class="dropdown-item" size="3" aria-label="size 3 select example">
+                            <option selected>teste1</option>
+                            <option value="1">teste2</option>
+                            <option value="1">teste3</option>
+                        </select>
+                    </div>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#"><img class="icones" src="../imagens/sair.png" alt="">Sair</a>
+                </li>
+            </ul>
+        </div>
+            <div id="barra-direita">
+                <input id="campo-pesquisa" type="text" name="" id="">
+                <img class="icones-menu" src="../imagens/lupa.png" alt="">
+            </div>
     </div>
     <div id="catalogo">
         <?php
